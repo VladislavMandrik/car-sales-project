1d3fcf13645452ac61702f473f666946f47cd4e0
VladislavMandrik <76885776+VladislavMandrik@users.noreply.github.com>
2021-05-21 21:09:08 +0300

    add Security.

---
+++ src/main/java/com/example/demo/config/CustomWebSecurityConfigurerAdapter.java
@@ -0,0 +1,56 @@
+package com.example.demo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+public class CustomWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private MyBasicAuthenticationEntryPoint authenticationEntryPoint;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth.inMemoryAuthentication().withUser("user")
+                .password(passwordEncoder().encode("password")).roles("USER")
+                .and().withUser("admin")
+                .password(passwordEncoder().encode("admin")).roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/api/v1/classifications").hasRole("ADMIN")
+                .antMatchers(HttpMethod.PUT, "/api/v1/classifications/*").hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/api/v1/classifications/*").hasRole("ADMIN")
+                .antMatchers(HttpMethod.POST, "/api/v1/classifications/*/ads")
+                .hasRole("ADMIN")
+                .antMatchers(HttpMethod.PUT, "/api/v1/classifications/*/ads/*")
+                .hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/api/v1/classifications/*/ads/*")
+                .hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .httpBasic()
+                .authenticationEntryPoint(authenticationEntryPoint);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
